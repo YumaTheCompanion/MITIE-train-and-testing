15f680aa57486ff84bdf2307e4e6ab2f4576b66b
Okan Barut <26722429+YumaTheCompanion@users.noreply.github.com>
2019-10-04 14:19:57 +0300

    Update runner.sh
    Must record output because in the end can't see what value fscore converges

--- how-to-run/runner.sh
+++ how-to-run/runner.sh
@@ -2,11 +2,14 @@
 
 make;
 [ $? -eq 0 ]  || exit 1
-./train_ner_example ../../../MITIE-models/english/total_word_feature_extractor.dat;
+./train_ner_example /home/okanb/MITIE/examples/cpp/train_ner/modifications.txt /home/okanb/MITIE/examples/cpp/train_ner/NER-datasets/CONLL2003/train.txt ../../../MITIE-models/english/total_word_feature_extractor.dat > train_output_results.txt;
 [ $? -eq 0 ]  || exit 1
-cat sample_text.txt | ../../../ner_stream new_ner_model.dat
+cat sample_text.txt | ../../../ner_stream new_ner_model.dat > new_ner_model_analysis.txt
 [ $? -eq 0 ]  || exit 1
 
-echo "\n\nHere is the original result\n\n"
+cat sample_text.txt | ../../../ner_stream new_ner_model_0001.dat > new_ner_model_analysis_0001.txt
+
+echo "\n\nvs original data\n\n"
 
 cat sample_text.txt | ../../../ner_stream ../../../MITIE-models/english/ner_model.dat;
+[ $? -eq 0 ]  || exit 1
